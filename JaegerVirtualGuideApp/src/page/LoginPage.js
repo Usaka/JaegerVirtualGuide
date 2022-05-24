a6a377aaa837b498527cb25c5811eaa91b6123ac
Camila Perdomo <camila.perdomo@uao.edu.co>
2022-05-23 23:32:01 -0500

    ajuste genral para conexión al servidor

--- JaegerVirtualGuideApp/src/page/LoginPage.js
+++ JaegerVirtualGuideApp/src/page/LoginPage.js
@@ -2,47 +2,47 @@
  * Componente que permite el logueo de un usuario pr medio de Username y Contraseña
  */
 
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import {
-    StyleSheet,
-    View,
-    Image,
+  StyleSheet,
+  View,
+  Image,
 } from 'react-native';
 
-import LoginForm from '../components/LoginComponents/LoginForm'
+import LoginForm from '../components/LoginComponents/LoginForm';
 
-//Crea el compónente de Login para permitir el ingreso del usuario
-const LoginPage = ({ setIsLogin, setUserData }) => {
-    const [loginData, setLoginData] = useState(undefined)
+// Crea el compónente de Login para permitir el ingreso del usuario
+function LoginPage({ setIsLogin, setUserData }) {
+  const [loginData, setLoginData] = useState(undefined);
 
-    return (
-        <View style={styles.containerLogin}>
-            {/* Logo de la app */}
-            <Image
-                style={styles.logo}
-                resizeMode="cover"
-                source={require('../assets/images/Logo.png')}
-            />
-            {/* Formulario para el login */}
-            <LoginForm
-                setLoginData={setLoginData}
-            />
-        </View>
-    )
+  return (
+    <View style={styles.containerLogin}>
+      {/* Logo de la app */}
+      <Image
+        style={styles.logo}
+        resizeMode="cover"
+        source={require('../assets/images/Logo.png')}
+      />
+      {/* Formulario para el login */}
+      <LoginForm
+        setLoginData={setLoginData}
+      />
+    </View>
+  );
 }
 
 // Variable que contiene los estilos de la vista
 const styles = StyleSheet.create({
-    containerLogin: {
-        flex: 1,
-        justifyContent: 'center',
-        alignContent: 'center',
-    },
-    logo: {
-        alignSelf: 'center',
-        width: 200,
-        height: 200,
-    }
-})
+  containerLogin: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+  logo: {
+    alignSelf: 'center',
+    width: 200,
+    height: 200,
+  },
+});
 
-export default LoginPage
+export default LoginPage;
